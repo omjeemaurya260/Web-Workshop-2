53e63483feeac60b66087a802c4032af7b58aa47
Om Jee Maurya <omjeemaurya1@gmail.com>
2026-03-17 01:30:49 +0530

    java script of result calculater

---
+++ Experiment-4/index.js
@@ -0,0 +1,35 @@
+function calculateResult(){
+let n=document.getElementById("subject").value;
+let total=0;
+for(let i=1;i<=n;i++){
+    let marks=parseFloat(prompt("Enter marks of subject "+ i+":"));
+    total=total+marks;
+}
+let average=total/n;
+let grade;
+let result;
+if(average>=90){
+    grade="A+";
+}
+else if(average>=75){
+    grade="A";
+}
+else if(average>=55){
+    grade="B";
+}
+else if(average>=40){
+    grade="c";
+}
+else{
+    grade="F";
+}
+if(average>=45){
+    result="pass";
+}else{
+    result="fail"
+}
+document.getElementById("result").innerHTML="Total Marks: "+total+
+"<br>Grade: "+grade+
+"<br>Average: "+average+
+"<br>Result: "+result;
+}
